ef31b43c798c2c346521d7f3787ec3c9114338ab
Kudrot467 <kkudrotekhoda@gmail.com>
2024-01-30 13:28:06 +0600

    for,while,do-while loop added

---
+++ loops.js
@@ -0,0 +1,14 @@
+let x=['a','b','c','d','e'];
+for (let i=0;i<3;i++){
+    console.log(x[i]);
+}
+let w=0;
+while(w<4){
+    console.log(x[w]);
+    w++;
+}
+let j=0;
+do{
+    console.log(x[j]);
+    j++;
+}while(j<2);
