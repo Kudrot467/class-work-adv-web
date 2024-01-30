8c8227930c16561d5c754b3d1350c132f91f4e66
Kudrot467 <kkudrotekhoda@gmail.com>
2024-01-30 13:11:43 +0600

    control flow switch example added

--- controlFlow.js
+++ controlFlow.js
@@ -6,3 +6,16 @@ if(age >= 18){
 }else{
     console.log("Minor");
 }
+let age2=19;
+let status;
+switch(true){
+    case age2 >=18:
+        status="Adult";
+        break;
+    case age2 >=13:
+        status="Teenager";
+        break;
+        default:
+            status="Minor";
+}
+console.log(status);
